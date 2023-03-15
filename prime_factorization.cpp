1f9a6e6afc3c2f4523b9746186ce1ed64dc45656
Haoran Peng <gavinsweden@gmail.com>
2023-03-15 11:36:16 +0000

    improve prime factorization and primality test

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "prime_factorization.h"
+#include "sieve.h"
 #include "utils.h"
 
 
 
 int main(int argc, char const *argv[])
 {	
-	std::uint64_t n = std::stoull(argv[1]);
-	auto v = trial_division(n);
-	print_vector(v);
+	constexpr int limit = 10000000;
+	const auto s = sieve(limit);
+	for (int i = 0; i <= limit; ++i) {
+		if (s[i] != is_prime(i)) std::cout << "bug" << std::endl;
+	}
+	print_vector(trial_division(95384));
 	return 0;
 }
--- prime_factorization.h
+++ prime_factorization.h
@@ -17,9 +17,26 @@ inline std::vector<std::uint64_t> trial_division(std::uint64_t n) {
 			ans.push_back(f);
 			n /= f;
 		} else {
-			++f;
+			f += 2;
 		}
 	}
 	if (n != 1) ans.push_back(n);  // n is prime
 	return ans;
+}
+
+
+inline bool is_prime(std::uint64_t n) {
+	// n is the integer to be factored.
+	// only need to test factors f <= sqrt(n)
+	// because if n is divisible by p, then n = p * q
+	// if p > q, n would have been reduced to n / q already.
+	if (n < 2) return false;
+	if (n == 2) return true;
+	if (n % 2 == 0) return false;
+	std::uint64_t f = 3;
+	while (f * f <= n) {
+		if (n % f == 0) return false;
+		f += 2;
+	}
+	return true;
 }
